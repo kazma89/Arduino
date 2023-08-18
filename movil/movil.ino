e1471e24df98d5b1095730ab2f1461f039c83b41
Randy Douglas <randyweb20@gmail.com>
2023-08-18 13:13:02 -0600

    Update movil.ino

--- movil/movil.ino
+++ movil/movil.ino
@@ -24,3 +24,68 @@ void loop() {
   }
   delay(timer);  
 }
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial sim900(0, 1); // RX, TX (conexión a la tarjeta ICOM SAT 1.1)
+
+void setup() {
+  Serial.begin(9600);
+  sim900.begin(9600);
+
+  Serial.println("Configurando modulo SIM900...");
+  delay(2000);
+  
+  // Verificar la conexión con el módulo SIM900
+  if (sim900.available()) {
+    Serial.println("Modulo SIM900 conectado correctamente.");
+  } else {
+    Serial.println("Error al conectar con el modulo SIM900.");
+  }
+  
+  delay(1000);
+}
+
+void loop() {
+  // Enviar comando AT para verificar la comunicación con el módulo SIM900
+  sim900.println("AT");
+  delay(1000);
+  
+  while (sim900.available()) {
+    char c = sim900.read();
+    Serial.write(c);
+  }
+  
+  // Configurar el modo de texto para enviar mensajes SMS
+  sim900.println("AT+CMGF=1");
+  delay(1000);
+  
+  // Ingresar el número de teléfono al que se enviará el mensaje
+  String numeroDestino = "+506xxxxxxxx"; // Reemplazar con el número de Costa Rica
+  
+  // Mensaje a enviar
+  String mensaje = "Hola desde Arduino!";
+  
+  // Componer el comando para enviar el mensaje
+  String comandoMensaje = "AT+CMGS=\"" + numeroDestino + "\"";
+  
+  // Enviar el comando para iniciar el mensaje
+  sim900.println(comandoMensaje);
+  delay(1000);
+  
+  // Enviar el contenido del mensaje
+  sim900.println(mensaje);
+  delay(100);
+  
+  // Enviar Ctrl+Z para finalizar el mensaje
+  sim900.println((char)26);
+  delay(1000);
+  
+  // Mostrar respuesta del módulo SIM900
+  while (sim900.available()) {
+    char c = sim900.read();
+    Serial.write(c);
+  }
+  
+  delay(5000); // Esperar antes de enviar otro mensaje
+}
